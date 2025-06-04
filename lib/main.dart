c74a0f645f961c0cb91f0b81f975dcf461e5df2b
HitzCommitted <147034456+HitzCommitted@users.noreply.github.com>
2025-06-04 14:05:15 +0300

    basic working screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_quiz_app/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: StartScreen())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 0, 204, 153), // Emerald Green
+                Color.fromARGB(255, 0, 102, 102), // Teal Shadow
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
