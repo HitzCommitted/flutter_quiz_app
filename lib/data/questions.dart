7b070a24bb24e002bc19c61d7af740ee680a8cbf
HitzCommitted <147034456+HitzCommitted@users.noreply.github.com>
2025-06-06 02:05:32 +0300

    basic working questions page

---
+++ lib/data/questions.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion('Which ancient city was buried under volcanic ash in 79 AD?', [
+    'Pompeii',
+    'Athens',
+    'Carthage',
+    'Babylon',
+  ]),
+  QuizQuestion('What’s the only letter not appearing in any U.S. state name?', [
+    'Q',
+    'Z',
+    'X',
+    'J',
+  ]),
+  QuizQuestion('Which planet rains diamonds due to extreme pressure?', [
+    'Neptune',
+    'Earth',
+    'Mars',
+    'Venus',
+  ]),
+  QuizQuestion('Who was the first human to journey into outer space?', [
+    'Yuri Gagarin',
+    'Neil Armstrong',
+    'Buzz Aldrin',
+    'Alan Shepard',
+  ]),
+  QuizQuestion(
+    'What animal can survive being frozen solid and thawed back to life?',
+    ['Wood frog', 'Polar bear', 'Camel', 'Penguin'],
+  ),
+  QuizQuestion(
+    'Which book starts with the line “It was the best of times, it was the worst of times”?',
+    ['A Tale of Two Cities', 'Moby Dick', 'The Great Gatsby', '1984'],
+  ),
+];
