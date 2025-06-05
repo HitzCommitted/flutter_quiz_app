4de1bc544bf89c301271977bdbbe195be4cabe29
HitzCommitted <147034456+HitzCommitted@users.noreply.github.com>
2025-06-05 14:11:59 +0300

    save point

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -12,19 +12,19 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 350,
-            color: Color.fromARGB(150, 255, 255, 255), // Semi-transparent white
+            color: Color.fromARGB(200, 250, 250, 250), // Subtle off-white
           ),
           SizedBox(height: 25),
           const Text(
             'Learn Flutter the fun way!',
-            style: TextStyle(color: Colors.white, fontSize: 28),
+            style: TextStyle(color: Colors.white, fontSize: 30),
           ),
           SizedBox(height: 20),
           OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            icon: const Icon(Icons.arrow_right_alt),
-            label: const Text('Start Quiz'),
+            icon: const Icon(Icons.arrow_right_alt, size: 34),
+            label: const Text('Start Quiz', style: TextStyle(fontSize: 24)),
           ),
         ],
       ),
