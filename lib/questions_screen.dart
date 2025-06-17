b72355b683af1c653d1f50ad26c42afb220b8db5
HitzCommitted <147034456+HitzCommitted@users.noreply.github.com>
2025-06-17 08:08:22 +0300

    working dynamic answer buttons

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz_app/answer_button.dart';
+import 'package:flutter_quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,19 +14,22 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question...',
-            style: TextStyle(fontSize: 24, color: Colors.white),
+          Text(
+            currentQuestion.text,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 24, color: Colors.white),
           ),
           const SizedBox(height: 30),
-          AnswerButton(answerText: 'Answer 1', onTap: () {}),
-          AnswerButton(answerText: 'Answer 2', onTap: () {}),
-          AnswerButton(answerText: 'Answer 3', onTap: () {}),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          }),
         ],
       ),
     );
